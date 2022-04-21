bbfb3b3577dad5f254c7ad0a41674d7745cca2d3
dsapedro <91165847+dsapedro@users.noreply.github.com>
2022-04-20 21:16:24 -0300

    Add files via upload

---
+++ Exemplo Builder/CarroProduct.java
@@ -0,0 +1,10 @@
+package com.mycompany.demo.cliente;
+
+public class CarroProduct {
+
+    public double preco;
+    public String dscMotor;
+    public int anoDeFabricacao;
+    public String modelo;
+    public String montadora;
+}
---
+++ Exemplo Builder/ConcessionariaDirector.java
@@ -0,0 +1,22 @@
+package com.mycompany.demo.cliente;
+
+public class ConcessionariaDirector {
+
+    protected CarroBuilder montadora;
+
+    public ConcessionariaDirector(CarroBuilder montadora) {
+        this.montadora = montadora;
+    }
+
+    public void construirCarro() {
+        montadora.buildPreco();
+        montadora.buildAnoDeFabricacao();
+        montadora.buildDscMotor();
+        montadora.buildModelo();
+        montadora.buildMontadora();
+    }
+
+    public CarroProduct getCarro() {
+        return montadora.getCarro();
+    }
+}
